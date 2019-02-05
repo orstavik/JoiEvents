2f142f31f8dc4eccc1a26e71f46e2ac34e0d0db4
intertext.no <orstavik77@gmail.com>
2019-02-05 11:24:31 +0100

    updating prettyprinter

--- src/webcomps/PrettyPrinter.js
+++ src/webcomps/PrettyPrinter.js
@@ -1,17 +1,21 @@
-function PRready(cb){
-  window.PR === undefined ?
-    setTimeout(()=> PRready(cb), 200) :
+function PRready(delay, cb) {
+  if (window.PR === undefined)
+    return setTimeout(() => PRready(delay, cb));
   cb();
 }
 
-const pp = document.createElement("script");
-pp.src= "https://cdn.rawgit.com/google/code-prettify/master/src/prettify.js";
-document.body.appendChild(pp);
+function addSyncScript(src){
+  const pp = document.createElement("script");
+  pp.src = src;
+  document.body.appendChild(pp);
+}
+
+addSyncScript("https://cdn.rawgit.com/google/code-prettify/master/src/prettify.js");
 
 class PrettyPrinter extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    PRready(()=> console.log("PR"));
+    PRready(() => console.log("PR"));
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML = `
 <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/google/code-prettify/master/src/prettify.css">
@@ -25,6 +29,7 @@ class PrettyPrinter extends HTMLElement {
   }
   .extra {
     background: white;
+    border: 2px dotted grey;
   }
   .com:hover {
     cursor: help;
@@ -38,66 +43,98 @@ we don't want to do this, as it would load the script in every web component.
     this.toggleCommentClosure = this.toggleComment.bind(this);
   }
 
-  connectedCallback(){
+  connectedCallback() {
     this.shadowRoot.addEventListener("click", this.toggleCommentClosure);
   }
 
-  disconnectedCallback(){
+  disconnectedCallback() {
     this.shadowRoot.removeEventListener("click", this.toggleCommentClosure);
   }
 
-  static get observedAttributes(){
+  static get observedAttributes() {
     return ["href"];
   }
 
-  async attributeChangedCallback(name, oldVal, newVal){
+  async attributeChangedCallback(name, oldVal, newVal) {
     if (name !== "href")
       return;
     var data = await fetch(newVal);
     var text = await data.text();
-    PRready(() => this.shadowRoot.children[1].innerHTML = PR.prettyPrintOne(text));
+    PRready(200, () => {
+      this.shadowRoot.children[1].innerHTML = PR.prettyPrintOne(text);
+      let i = 1;
+      let comment = getCommentText(this.shadowRoot, i);
+      while (comment) {
+        let div = makeCommentDiv(i, comment);
+        this.shadowRoot.children[1].appendChild(div);
+        comment = getCommentText(this.shadowRoot, ++i);
+      }
+    });
   }
 
-  toggleComment(e){
+  toggleComment(e) {
     var num = getCommentNumber(e.target);
     if (num === undefined)
       return;
-    var active = this.shadowRoot.querySelector("#extra"+num);
-    if (active)
-      return active.remove();
-    var addOnComment = this.getCommentText(num);
-    var commentDiv = makeCommentDiv(num, addOnComment, e.offsetX, e.offsetY);
-    this.shadowRoot.appendChild(commentDiv);
-  }
-
-  getCommentText(num){
-    var regex = new RegExp("^\/\/("+ num + ")\\.\\s");
-    var coms = this.shadowRoot.querySelectorAll("span.com");
-    for (var i = 0; i < coms.length; i++) {
-      var com2 = coms[i];
-      var hit2 = regex.exec(com2.innerText);
-      if (hit2)
-        return com2.innerText.substr(hit2[0].length);
-    }
-    return null;
+    var active = this.shadowRoot.querySelector("#extra" + num);
+    if (!active)
+      return;
+    active.style.left = e.offsetX + "px";
+    active.style.top = e.offsetY + "px";
+    active.style.display === "none" ?
+      active.style.display = "block" :
+      active.style.display = "none";
   }
 }
 
-function makeCommentDiv(num, addOnComment, x, y){
+function getCommentText(root, num) {
+  var regex = new RegExp("^\/\/(" + num + ")\\.\\s");
+  var coms = root.querySelectorAll("span.com");
+  for (var i = 0; i < coms.length; i++) {
+    var com2 = coms[i];
+    var hit2 = regex.exec(com2.innerText);
+    if (hit2) {
+      return getCommentTextLines(com2);
+    }
+  }
+  return null;
+}
+
+function getCommentTextLines(com2) {
+  let res = [com2];
+  var regex = new RegExp("^\/\/\\s\\s\\s");
+  while (
+    com2.nextSibling &&
+    com2.nextSibling.nextSibling &&
+    com2.nextSibling.nextSibling.classList.contains("com") &&
+    regex.exec(com2.nextSibling.nextSibling.innerText)
+    ) {
+    res.push(document.createElement("br"));
+    res.push(com2.nextSibling.nextSibling);
+    com2 = com2.nextSibling.nextSibling;
+  }
+  return res;
+}
+
+function makeCommentDiv(num, addOnComment, x, y) {
   var div = document.createElement("div");
-  div.id = "extra"+num;
-  div.innerText = addOnComment;
+  div.id = "extra" + num;
+
+  for (let i = 0; i < addOnComment.length; i++)
+    div.appendChild(addOnComment[i]);
   div.classList.add("extra");
   div.style.position = "absolute";
+  div.style.display = "none";
   div.style.top = y + "px";
   div.style.left = x + "px";
   return div;
 }
 
-function getCommentNumber(el){
+function getCommentNumber(el) {
   if (!el.classList.contains("com"))
     return;
   var hit = /^\/\/\[(\d+)\]/.exec(el.innerText);
   return hit ? hit[1] : undefined;
 }
+
 customElements.define("pretty-printer", PrettyPrinter);
