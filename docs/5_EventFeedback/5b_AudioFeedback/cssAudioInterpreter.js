3abc21a6009d70da76da36a6f7e43c03cefd5b8d
intertext.no <orstavik77@gmail.com>
2019-09-29 11:16:08 +0200

    interpreter architecture work

--- docs/5_EventFeedback/5b_AudioFeedback/cssAudioInterpreter.js
+++ docs/5_EventFeedback/5b_AudioFeedback/cssAudioInterpreter.js
@@ -44,91 +44,100 @@ class InterpreterFunctions {
   }
 
   static async url(audioCtx, url) {
-    var bufferSource = audioCtx.createBufferSource();
-    var file = await fetch(url);
-    var data = await file.arrayBuffer();
+    const data = await InterpreterFunctions.getFileBuffer(url);
+    const bufferSource = audioCtx.createBufferSource();
     audioCtx.decodeAudioData(data, decodedData => bufferSource.buffer = decodedData);
     return bufferSource;
   }
+
+  //todo cache this buffer in the bufferMap
+  static async getFileBuffer(url) {
+    var file = await fetch(url);
+    return await file.arrayBuffer();
+  }
 }
 
-function connectMtoN(m, n) {
-  for (let a of m) {
-    for (let b of n) {
-      if (!a instanceof AudioNode)
-        throw new SyntaxError("CssAudioNode cannot be: " + a);
-      if (!b instanceof AudioNode)
-        throw new SyntaxError("CssAudioNode cannot be: " + b);
-      a.connect(b);
-      let inputs = b.inputs || (b.inputs = []);
-      inputs.push(a);
+const bufferMap = {};
+
+class CssAudioInterpreterContext {
+
+  static connectMtoN(m, n) {
+    for (let a of m) {
+      for (let b of n) {
+        if (!a instanceof AudioNode)
+          throw new SyntaxError("CssAudioNode cannot be: " + a);
+        if (!b instanceof AudioNode)
+          throw new SyntaxError("CssAudioNode cannot be: " + b);
+        a.connect(b);
+        let inputs = b.inputs || (b.inputs = []);
+        inputs.push(a);
+      }
     }
   }
-}
 
-async function interpretPipe(pipe) {
-  const nodes = [];
-  for (let node of pipe.nodes) {
-    node = await interpretNode(node);
-    node = node instanceof Array ? node : [node];
-    nodes.push(node);
-  }
-  for (let i = 0; i < nodes.length - 1; i++)
-    connectMtoN(nodes[i], nodes[i + 1]);
-  return nodes.pop();
-}
-
-/**
- * todo 0. Can we reuse reuse the audio node or context?
- * todo 1. cache the url audio in some way, so that it doesn't need to fetch it anymore? keep the sound in memory. should/can this be done to all audio nodes? can i clone an audio node?
- * todo a. make this recursive instead? first pipe, then array, then node, then argument? but I don't need argument, as it has already been processed?
- *
- * @param node
- * @returns {Promise.<*>}
- */
-async function interpretNode(node) {
-  if (node.type === "pipe")
-    return await interpretPipe(node);
-  if (node instanceof Array) {
-    const res = [];
-    for (let item of node) {
-      item = await interpretNode(item);
-      res.push(item);
+  static async interpretPipe(ctx, pipe) {
+    const nodes = [];
+    for (let node of pipe.nodes) {
+      node = await CssAudioInterpreterContext.interpretNode(ctx, node);
+      node = node instanceof Array ? node : [node];
+      nodes.push(node);
     }
-    return res;
+    for (let i = 0; i < nodes.length - 1; i++)
+      CssAudioInterpreterContext.connectMtoN(nodes[i], nodes[i + 1]);
+    return nodes.pop();
   }
-  if (node.type === "fun") {
-    const args = [];
-    for (let item of node.args) {
-      item = await interpretNode(item);
-      args.push(item);
+
+  /**
+   * todo 0. Can we reuse reuse the audio node or context?
+   * todo 1. cache the url audio in some way, so that it doesn't need to fetch it anymore? keep the sound in memory. should/can this be done to all audio nodes? can i clone an audio node?
+   * todo a. make this recursive instead? first pipe, then array, then node, then argument? but I don't need argument, as it has already been processed?
+   *
+   * @param node
+   * @returns {Promise.<*>}
+   */
+  static async interpretNode(ctx, node) {
+    if (node.type === "pipe")
+      return await CssAudioInterpreterContext.interpretPipe(ctx, node);
+    if (node instanceof Array) {
+      const res = [];
+      for (let item of node) {
+        item = await CssAudioInterpreterContext.interpretNode(ctx, item);
+        res.push(item);
+      }
+      return res;
     }
-    return await makeNode(node.name, args);
+    if (node.type === "fun") {
+      const args = [];
+      for (let item of node.args) {
+        item = await CssAudioInterpreterContext.interpretNode(ctx, item);
+        args.push(item);
+      }
+      return await CssAudioInterpreterContext.makeNode(ctx, node.name, args);
+    }
+    if (typeof node === "string")
+      return await CssAudioInterpreterContext.makeNode(ctx, node);
+    throw new Error("omg? wtf? " + node)
   }
-  if (typeof node === "string")
-    return await makeNode(node);
-  throw new Error("omg? wtf? " + node)
-}
 
-async function makeNode(name, args) {
-  return InterpreterFunctions[name] ? await InterpreterFunctions[name](audioCtx, ...args) : name;
-}
-
-let audioCtx;
-
-function startNodes(node) {
-  if (node.inputs) {
-    for (let child of node.inputs)
-      startNodes(child);
+  static async makeNode(ctx, name, args) {
+    return InterpreterFunctions[name] ? await InterpreterFunctions[name](ctx, ...args) : name;
   }
-  node.start && node.start();
+
+  static startNodes(node) {
+    if (node.inputs) {
+      for (let child of node.inputs)
+        CssAudioInterpreterContext.startNodes(child);
+    }
+    node.start && node.start();
+  }
 }
 
-export async function interpret(str, ctx) {
-  audioCtx = ctx;
+export async function interpret(str, ctx, startImmediately) {
+  if (!startImmediately)
+    ctx.suspend();
   const ast = parse(str);
-  const audioNodes = await interpretPipe(ast);
-  connectMtoN(audioNodes, [ctx.destination]);
-  startNodes(ctx.destination);
+  const audioNodes = await CssAudioInterpreterContext.interpretPipe(ctx, ast);
+  CssAudioInterpreterContext.connectMtoN(audioNodes, [ctx.destination]);
+  CssAudioInterpreterContext.startNodes(ctx.destination);
   return audioNodes;
 }
