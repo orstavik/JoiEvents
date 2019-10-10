9b8d77c8263339a242b5e83cfecb187dc9dc6373
intertext.no <orstavik77@gmail.com>
2019-10-10 16:16:40 +0200

    update tests. Which is bad. Should just make it into a mocha chai test asap.

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/syntaxTest.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/syntaxTest.js
@@ -2,16 +2,16 @@ import {parse} from "../Parser.js";
 
 describe('pipe', function () {
   it("a > b > c", function () {
-    const tst  = parse('a > b > c');
+    const tst = parse('a > b > c');
     const result = JSON.parse('{"type":">","args":[{"type":"a"},{"type":"b"},{"type":"c"}]}');
     expect(tst).to.deep.equal(result);
   });
 });
 
 describe('--css-var', function () {
-  it("a > b > c", function () {
-    const tst  = parse('a > b > c');
-    const result = JSON.parse('{"type":">","args":[{"type":"a"},{"type":"b"},{"type":"c"}]}');
+  it('a > --audio-var > c', function () {
+    const tst = parse('a > --audio-var > c');
+    const result = JSON.parse('{"type":">","args":[{"type":"a"},{"type":"--","varName":"--audio-var"},{"type":"c"}]}');
     expect(tst).to.deep.equal(result);
   });
 });
