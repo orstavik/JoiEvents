21606ebb338b8b8ed2ba6f6cb626fb04b71e5efd
intertext.no <orstavik77@gmail.com>
2019-10-18 09:17:59 +0200

    babystep friday fix ":"

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/LibSyntax.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/LibSyntax.js
@@ -1,44 +1,12 @@
 export const ListOps = Object.create(null);
-ListOps.topDown = {"+": 1, "-": 1};
-
-// ListOps["()"] = function (node) {
-//   return {type: "()", body: ListOps["[]"](node)};
-// };
-//
-// function commaTreeToArray(body) {
-//   const res = [];
-//   let ops = [];
-//   for (let root = body; root !== undefined; root = root.right) {
-//     ops.push(root);
-//     if (root.type === ",") {
-//       res.push(ops);
-//       ops = [];
-//     }
-//   }
-//   res.push(ops);
-//   return res;
-// }
-//
-// ListOps["[]"] = function ({body}) {
-//   if (!body)
-//     return [];
-//   if (!body.left && !body.right)            //no operators, single body
-//     return [body];
-//   const res = commaTreeToArray(body);
-//   for (let i = 0; i < res.length - 1; i++) {
-//     const row = res[i].map(node => Object.assign({}, node));
-//     if (row[row.length - 1].type === ",")                   //replace the comma with the left side of the comma
-//       row[row.length - 1] = row[row.length - 1].left;
-//     for (let j = 0; j < row.length - 1; j++)                //update the links in the clone that is needed to avoid mutations
-//       row[j].right = row[j + 1];
-//     res[i] = row;
-//   }
-//   return res.map(row => row[0]);
-// };
+ListOps.topDown = {":": 1};
 
 ListOps[":"] = function ({left, right}) {
-  if (!right || !(right instanceof Array))
-    return [left, right];
-  right.unshift(left);
-  return right;
+  const res = [left];
+  while (right && right.type === ":"){
+    res.push(right.left);
+    right = right.right;
+  }
+  res.push(right);
+  return res;
 };
