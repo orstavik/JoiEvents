a93d91d3afd5377962f76c5347e37f17b62931b7
intertext.no <orstavik77@gmail.com>
2019-10-18 14:42:44 +0200

    random

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/LibRandom.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/LibRandom.js
@@ -6,18 +6,25 @@ export const Random = {};
  * random(a, b, step) will return a random "step" between numbers "a" and "b".
  */
 Random.topDown = {};
-Random.random = function ({body: [a, b, steps]}) {
+Random.random = function (node) {
+  let {body: [a, b, steps]} = node;
   if (a instanceof Array)
     return a[Math.floor(Math.random() * a.length)];
+  if (a.num !== undefined)
+    a = a.num;
   if (typeof a === "number" && b === undefined && steps === undefined)
     return Math.random() * a;
+  if (b.num !== undefined)
+    b = b.num;
   if (typeof a === "number" && typeof b === "number" && steps === undefined)
     return (Math.random() * (b - a)) + a;
+  if (steps.num !== undefined)
+    steps = steps.num;
   if (steps < 0)
     throw new SyntaxError("Random function broken, illegal parameter: step must be a positive number" + steps);
   if (typeof a === "number" && typeof b === "number" && typeof steps === "number")
     return (Math.round((Math.random() * (b - a)) / steps) * steps) + a;
-  if (a === undefined  && typeof b === "number" && typeof steps === "number")
+  if (a === undefined && typeof b === "number" && typeof steps === "number")
     return Math.round((Math.random() * b) / steps) * steps;
   throw new SyntaxError("Random function broken, illegal parameters: " + n);
 };
