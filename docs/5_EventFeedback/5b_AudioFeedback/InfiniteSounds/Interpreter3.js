5ec5f2ac18ffb54a8f6c0e568c77a7d0f51355bc
intertext.no <orstavik77@gmail.com>
2019-10-21 10:21:31 +0200

    poc clef chain

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/Interpreter3.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/Interpreter3.js
@@ -47,13 +47,7 @@ export async function interpretNode(node, table, ctx) {
 
 export async function staticInterpret(str) {
   let node = parse(str);
-  //todo merge tables. Because I need to process the operators in the correct order.
-  //todo, when I have operator graph, I should rearrange it grammatically, so that the bottom nodes are the ones being processed first.
-  //todo, then, I can just run a bottom up pass on everything.
-  //todo, yes, keep the structure smart.
-  node = await interpretNode(node, ListOps);
-  node = await interpretNode(node, MathOps);
-  node = await interpretNode(node, Music, {activeKeys: new Set()});
+  node = await interpretNode(node, Object.assign({}, ListOps, MathOps, Music));
   //variables: declare and replace
   //cache temporarily
   return node;
@@ -61,10 +55,6 @@ export async function staticInterpret(str) {
 
 export async function interpret(str, ctx) {
   let node = await staticInterpret(str);
-  //todo no, cannot merge tables. Because I need to process the operators in the correct order.
-  //todo, when I have operator graph, I should rearrange it grammatically, so that the bottom nodes are the ones being processed first.
-  //todo, then, I can just run a bottom up pass on everything.
-  //todo, yes, keep the structure smart.
   node = await interpretNode(node, Random);
   node = await interpretNode(node, MathOps);
   node = await interpretNode(node, InterpreterFunctions, ctx);
