532ae0904b87a26fd344fe595d0e7dee318b925f
intertext.no <orstavik77@gmail.com>
2019-10-22 15:32:14 +0200

    preparing for music magic

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/Interpreter3.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/Interpreter3.js
@@ -15,29 +15,43 @@ async function interpretArray(node, table, ctx) {
   return clone;
 }
 
-//todo
-//1. what about the isPrimitive property on arrays.. first, invert the setting to check if it is primitive or not, and then try to make it into an object instead?
-//2. then pass in the parents as the root context, as an array of context nodes?
-
 export async function interpretNode(node, table, ctx) {
   if (isPrimitive(node))
     return node;
   if (node instanceof Array)
-    return await interpretArray(node, table, ctx);
+    return await interpretArray(node, table, ctx.concat([node]));
+  //todo here I can do a topDown pass
   const clone = Object.assign({}, node);
-  if (!isPrimitive(clone.body))
-    clone.body = await interpretArray(clone.body, table, ctx);
+  clone.body = await interpretArray(clone.body, table, ctx.concat([node]));
   const fun = table[clone.type];
-  return fun ? (await fun(clone, ctx)) : clone;
+  return fun ? (await fun(clone, ctx[0])) : clone;
 }
 
 //todo process unit functions such as "MHz", "dB", "ms". "b" (beats), and tones must be processed dynamically.
 const staticTable = Object.assign({}, ListOps, MathOps, Music);
+//todo I do a static pass bottom up for the music operators.
+//todo here I only register the properties that the children will look for later.
+//todo during the dynamic pass/hookup, I need to get nodes that are not yet added.
+
+//a) static pass up: each clef node is given a ".clef = []" table.
+//b) dynamic pass up:
+// A) clef nodes look at their .clef table. If this clef table only contains one other element,
+// the clef node adds its mathematics to the child clef/note and return its child (ie. removes itself).
+// and returns
+// the clef table has twelve rows. one for each tone in the scale. It will only create as many gainNodes as it needs.
+
+// B1) notes AND clefs with more than two dependencies create their own GainNode.
+//   They set the value of this gain node to be the mathematical alteration of the notes.
+//   they add their GainNode as the ".toneNode".
+// B2) they then go to their .clef table and connect their ".toneNode" to all their tone children's ".toneNode"s.
+// B3) they then search up the tree to find their parent clef. If a clef is found, they register themselves.
+//    If not found, they create their own constant node and then fill it with their ConstantSourceNode with the given key as source, or C4 if
+//    no key is given.
 const dynamicTable = Object.assign({}, Random, MathOps, InterpreterFunctions, AudioPiping);
 
 export async function staticInterpret(str) {
   let node = parse(str);
-  node = await interpretNode(node, staticTable, undefined);
+  node = await interpretNode(node, staticTable, []);
   //variables: declare and replace
   //cache temporarily
   return node;
@@ -45,6 +59,6 @@ export async function staticInterpret(str) {
 
 export async function interpret(str, ctx) {
   let node = await staticInterpret(str);
-  node = await interpretNode(node, dynamicTable, ctx);
+  node = await interpretNode(node, dynamicTable, [ctx]);
   return node;
 }
