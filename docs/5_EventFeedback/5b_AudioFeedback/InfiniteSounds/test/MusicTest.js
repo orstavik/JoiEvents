ed4a545be8c23d5b3c21991343c0c3cdb500b0a5
intertext.no <orstavik77@gmail.com>
2019-10-24 13:02:45 +0200

    half way

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/MusicTest.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/MusicTest.js
@@ -2,116 +2,118 @@ import {parse} from "../Parser2.js";
 import {staticInterpret, interpret} from "../Interpreter3.js";
 
 describe('absolute notes', function () {
-  it("C#4", function () {
-    const tst = parse("C#4");
-    const result = {type: "note", body: ["c#", 4, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
-  it("A5", function () {
-    const tst = parse("A5");
-    const result = {type: "note", body: ["a", 5, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
+  // it("C#4", function () {
+  //   const tst = parse("C#4");
+  //   const result = {type: "note", body: ["c#", 4, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
+  // it("A5", function () {
+  //   const tst = parse("A5");
+  //   const result = {type: "note", body: ["a", 5, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
   it("Bb5", function () {
     const tst = parse("Bb5");
-    const result = {type: "note", body: ["bb", 5, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
+    const result = {type: "note", body: ["bb", 6, undefined]};
+    // bigDiff({a:1, b:2}, {b:2, c:3});
+    expect(tst).toEqual(result);
+    // expect(deepDiff(tst, result)).to.be.true;
   });
-  it("D#-2", function () {
-    const tst = parse("D#-2");
-    const result = {type: "note", body: ["d#", -2, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
-  it("E", function () {
-    const tst = parse("E");
-    const result = {type: "note", body: ["e", undefined, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
-  it("f#", function () {
-    const tst = parse("f#");
-    const result = {type: "note", body: ["f#", undefined, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
-  it("g0", function () {
-    const tst = parse("g0");
-    const result = {type: "note", body: ["g", 0, undefined]};
-    expect(deepDiff(tst, result)).to.be.true;
-  });
+  // it("D#-2", function () {
+  //   const tst = parse("D#-2");
+  //   const result = {type: "note", body: ["d#", -2, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
+  // it("E", function () {
+  //   const tst = parse("E");
+  //   const result = {type: "note", body: ["e", undefined, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
+  // it("f#", function () {
+  //   const tst = parse("f#");
+  //   const result = {type: "note", body: ["f#", undefined, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
+  // it("g0", function () {
+  //   const tst = parse("g0");
+  //   const result = {type: "note", body: ["g", 0, undefined]};
+  //   expect(deepDiff(tst, result)).to.be.true;
+  // });
 });
 
 describe('relative notes', function () {
-  it("~0", function () {
-    const tst = parse("~0");
-    expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "0"]})).to.be.true;
-  });
-
-  it("~-11", function () {
-    const tst = parse("~-11");
-    expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "-11"]})).to.be.true;
-  });
-
-  it("~+11", function () {
-    const tst = parse("~+11b");
-    expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "+11b"]})).to.be.true;
-  });
-
-  it("~10#", function () {
-    const tst = parse("~10#");
-    expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "10#"]})).to.be.true;
-  });
+  // it("~0", function () {
+  //   const tst = parse("~0");
+  //   expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "0"]})).to.be.true;
+  // });
+  //
+  // it("~-11", function () {
+  //   const tst = parse("~-11");
+  //   expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "-11"]})).to.be.true;
+  // });
+  //
+  // it("~+11", function () {
+  //   const tst = parse("~+11b");
+  //   expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "+11b"]})).to.be.true;
+  // });
+  //
+  // it("~10#", function () {
+  //   const tst = parse("~10#");
+  //   expect(deepDiff(tst, {type: "note", body: [undefined, undefined, "10#"]})).to.be.true;
+  // });
 });
 
 describe('static interpretation of clef', function () {
-  it("~(G4, sine(~2))", async function () {
-    const tst = await staticInterpret("~(G4, sine(~2))");
-    console.log(tst);
-    const res = {
-      "type": "~",
-      "body": [
-        {
-          "type": "note",
-          "body": [
-            "g",
-            4
-          ]
-        },
-        {
-          "type": "sine",
-          "body": [
-            {
-              "type": "note",
-              "body": [
-                "~",
-                "2"
-              ]
-            }
-          ]
-        }
-      ],
-      "clefKey": {
-        "type": "note",
-        "body": [
-          "g",
-          4
-        ]
-      },
-      "clef": {
-        "0": [],
-        "1": [],
-        "2": [],
-        "3": [],
-        "4": [],
-        "5": [],
-        "6": [],
-        "7": [],
-        "8": [],
-        "9": [],
-        "10": [],
-        "11": []
-      }
-    };
-    expect(deepDiff(tst, res)).to.be.true;
-  });
+  // it("~(G4, sine(~2))", async function () {
+  //   const tst = await staticInterpret("~(G4, sine(~2))");
+  //   console.log(tst);
+  //   const res = {
+  //     "type": "~",
+  //     "body": [
+  //       {
+  //         "type": "note",
+  //         "body": [
+  //           "g",
+  //           4
+  //         ]
+  //       },
+  //       {
+  //         "type": "sine",
+  //         "body": [
+  //           {
+  //             "type": "note",
+  //             "body": [
+  //               "~",
+  //               "2"
+  //             ]
+  //           }
+  //         ]
+  //       }
+  //     ],
+  //     "clefKey": {
+  //       "type": "note",
+  //       "body": [
+  //         "g",
+  //         4
+  //       ]
+  //     },
+  //     "clef": {
+  //       "0": [],
+  //       "1": [],
+  //       "2": [],
+  //       "3": [],
+  //       "4": [],
+  //       "5": [],
+  //       "6": [],
+  //       "7": [],
+  //       "8": [],
+  //       "9": [],
+  //       "10": [],
+  //       "11": []
+  //     }
+  //   };
+  //   expect(deepDiff(tst, res)).to.be.true;
+  // });
 });
 
 // describe('dynamic interpretation of clef', function () {
--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/deepDiff.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/test/deepDiff.js
@@ -1,68 +1,24 @@
-function deepDiff(objectA, objectB) {
+var display = document.createElement('pre');
+document.body.prepend(display);
 
-  let isNotSpecialObject = function (a) {
-    const c = a.constructor;
-    return c !== Number && c !== String && c !== Date && c !== RegExp && c !== Function && c !== Boolean;
-  };
-  var isObject = function (a) {
-    if (a === undefined)
-      return false;
-    return a.constructor == Object || isNotSpecialObject(a);
-  };
+function diffToHTML(one, other) {
+  let diff = JsDiff.diffJson(one, other);
+  let fragment = document.createDocumentFragment();
 
-  var isArray = function (a) {
-    return a.constructor == Array;
-  };
-  let diffEntry = function (path, a, b) {
-    path = path.map(seg => typeof seg === "number" ? `[${seg}]` : "." + seg)
-      .join("");
-    a = (a === undefined ? "__undefined__" : a);
-    b = (b === undefined ? "__undefined__" : b);
-    return {Property: path, ObjectA: a, ObjectB: b};
-  };
-  var compare = function (a, b, path) {
-    if (a === b || (Number.isNaN(a) && Number.isNaN(b)))
-      return [];
-    if (a === Function && b === Function)     //filter out functions
-      return [];
-    if (!a || !b || !a.constructor || !b.constructor)
-      return [diffEntry(path, a, b)];
-    if (isArray(a) && !isArray(b))
-      return [diffEntry(path, a, b)];
-    if (!isArray(a) && isArray(b))
-      return [diffEntry(path, a, b)];
-    if (isArray(a) && isArray(b)) {
-      let res = [];
-      for (var i = 0; i < a.length; i++) {
-        b.length > i ?
-          res = res.concat(compare(a[i], b[i], path.concat([i]))) :
-          res.push(diffEntry(path.concat([i]), a[i], "__void__"));
-      }
-      for (var i = a.length; i < b.length; i++)
-        res.push(diffEntry(path.concat([i]), "__void__", b[i]));
-      return res;
-    }
-    if (isObject(a) && !isObject(b))
-      return [diffEntry(path, a, b)];
-    if (!isObject(a) && isObject(b))
-      return [diffEntry(path, a, b)];
-    if (isObject(a) && isObject(b)) {
-      let res = [];
-      for (let prop in a) {
-        prop in b ?
-          res = res.concat(compare(a[prop], b[prop], path.concat([prop]))) :
-          res.push(diffEntry(path.concat([prop]), a[prop], "__void__"));
-      }
-      for (let prop in b)
-        !(prop in a) && res.push(diffEntry(path.concat([prop]), "__void__", b[prop]));
-      return res;
-    }
-    return [diffEntry(path, a, b)];
-  };
-
-  var propertyChanges = compare(objectA, objectB, []);
-
-  if (!propertyChanges.length)
-    return true;
-  return JSON.stringify(propertyChanges);
+  for (let part of diff) {
+    let color = part.added ? 'green' :        // green for additions, red for deletions
+      part.removed ? 'red' : 'grey';          // grey for common parts
+    let span = document.createElement('span');
+    span.style.color = color;
+    span.appendChild(document
+      .createTextNode(part.value));
+    fragment.appendChild(span);
+  }
+  return fragment;
 }
+
+window.bigDiff = function(a, b){
+  display.appendChild(diffToHTML(a, b));
+};
+
+// bigDiff('beep boop', 'beep boob blah');
