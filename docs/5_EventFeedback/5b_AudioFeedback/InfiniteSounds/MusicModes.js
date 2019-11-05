82c083e6b1eca29fb51fd5512077aced1fdedf6f
intertext.no <orstavik77@gmail.com>
2019-11-05 11:18:14 +0100

    simplified music modes

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/MusicModes.js
+++ docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/MusicModes.js
@@ -8,23 +8,9 @@ const modeVectors = [
   [0, 2, 4, 6, 7, 9, 11],
 ];
 
-const modeNameToVector = {
-  "loc": [0, 1, 3, 5, 6, 8, 10],
-  "phr": [0, 1, 3, 5, 7, 8, 10],
-  "aeo": [0, 2, 3, 5, 7, 8, 10],
-  "dor": [0, 2, 3, 5, 7, 9, 10],
-  "mix": [0, 2, 4, 5, 7, 9, 10],
-  "ion": [0, 2, 4, 5, 7, 9, 11],
-  "lyd": [0, 2, 4, 6, 7, 9, 11],
-};
-modeNameToVector["maj"] = modeNameToVector["ion"];
-modeNameToVector["min"] = modeNameToVector["aeo"];
-
-//freeze all mode vectors so noone alters them by mistake.
+//freeze all mode vectors so no one alters them by mistake.
 for (let vec of modeVectors)
   Object.freeze(vec);
-for (let name in modeNameToVector)
-  Object.freeze(modeNameToVector[name]);
 
 const modeNameToNumber = {
   "loc": 0,
@@ -34,18 +20,20 @@ const modeNameToNumber = {
   "mix": 4,
   "ion": 5,
   "lyd": 6,
+
+  "maj": 5,
+  "min": 2,
 };
-modeNameToNumber["maj"] = modeNameToNumber["ion"];
-modeNameToNumber["min"] = modeNameToNumber["aeo"];
 
 const modeNumberToName = ["loc", "phr", "aeo", "dor", "mix", "ion", "lyd"];
 
 export class MusicModes {
 
+  //defaults to major/ionian if not a valid name or number.
   static getVector(nameOrNum) {
-    return Number.isInteger(nameOrNum) ?
-      modeVectors[nameOrNum] :
-      modeNameToVector[nameOrNum];
+    typeof nameOrNum === "string" && (nameOrNum = modeNameToNumber[nameOrNum]);
+    nameOrNum === undefined && (nameOrNum = 5);           //undefined yields 5, major ionian
+    return modeVectors[nameOrNum];
   }
 
   static getName(num) {
