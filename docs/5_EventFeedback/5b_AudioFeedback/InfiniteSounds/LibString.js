0928ffb98da2f03bc2352dfdb0d94d96f7175b34
intertext.no <orstavik77@gmail.com>
2019-11-27 12:20:09 +0100

    moving out JoiMusic

--- docs/5_EventFeedback/5b_AudioFeedback/InfiniteSounds/LibString.js
+++
@@ -1,8 +0,0 @@
-export const StringOps = Object.create(null);
-
-StringOps["+"] = function (n) {
-  const [l, r] = n.body;
-  if (typeof l !== "string" && typeof r !== "string")   //if one is a string, then we concatenate
-    return n;
-  return l + r;
-};
