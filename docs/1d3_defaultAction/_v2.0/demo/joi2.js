ce53fb100d1822642bee383618856ad918bfd4ca
intertext.no <orstavik77@gmail.com>
2020-05-13 15:02:41 +0200

    adding slottablesEvent

--- docs/1d3_defaultAction/_v2.0/demo/joi2.js
+++ docs/1d3_defaultAction/_v2.0/demo/joi2.js
@@ -1,3 +1,6 @@
 import {} from "./nativeDefaultActions.js";
 import {} from "./JoiEventPatchNew.js";
 import {} from "../../../1b_EventLoop/demo/toggleTick.js";
+import {SlottablesEvent} from "./slottablesEvent.js";
+
+export const SlottablesEventMixin = SlottablesEvent;
