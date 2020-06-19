cca8d64b6b56753bb4fa5e1cd038668977d40466
intertext.no <orstavik77@gmail.com>
2020-06-19 10:26:33 +0200

    babystep ready for unit tests

--- docs/1d1_event_propagation/unit_test/testPropagation.js
+++ docs/1d1_event_propagation/unit_test/testPropagation.js
@@ -44,81 +44,121 @@ function cleanDom() {
   }
 }
 
+function addListenersToDOM(dom) {
+  for (let elName in dom) {
+    dom[elName].addEventListener("click", function (e) {
+      res1 += elName + " ";
+      res2 += "-";
+      res3 += e.eventPhase;
+    }, {});
+    dom[elName].addEventListener("click", function (e) {
+      res1 += elName + " ";
+      res2 += "+";
+      res3 += e.eventPhase;
+    }, true);
+  }
+}
+
 let res = "";
+let res1 = "";
+let res2 = "";
+let res3 = "";
+
+
 export const testProp = [{
   name: "propagation: composed: NO bubbles: NO",
   fun: function () {
-    res = "";
+    res1 = res2 = res3 = "";
     const dom = cleanDom();
-    for (let elName in dom) {
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "-" + e.eventPhase + " ";
-      }, {});
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "+" + e.eventPhase + " ";
-      }, true);
-    }
+    addListenersToDOM(dom);
     dom.shadowH1.dispatchEvent(new Event("click"));
   },
-  expect: "shadowRoot+1 shadowH1-2 shadowH1+2 ",
+  expect: "shadowRoot shadowH1 shadowH1 +-+:122",
   result: function () {
-    return res;
+    return res1 + res2 + ":" + res3;
   }
 }, {
   name: "propagation: composed: NO bubbles: YES",
   fun: function () {
-    res = "";
+    res1 = res2 = res3 = "";
     const dom = cleanDom();
-    for (let elName in dom) {
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "-" + e.eventPhase + " ";
-      }, {});
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "+" + e.eventPhase + " ";
-      }, true);
-    }
+    addListenersToDOM(dom);
     dom.shadowH1.dispatchEvent(new Event("click", {bubbles: true}));
   },
-  expect: "shadowRoot+1 shadowH1-2 shadowH1+2 shadowRoot-3 ",
+  expect: "shadowRoot shadowH1 shadowH1 shadowRoot +-+-:1223",
   result: function () {
-    return res;
+    return res1 + res2 + ":" + res3;
   }
 }, {
   name: "propagation: composed: YES bubbles: NO",
   fun: function () {
-    res = "";
+    res1 = res2 = res3 = "";
     const dom = cleanDom();
-    for (let elName in dom) {
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "-" + e.eventPhase + " ";
-      }, {});
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "+" + e.eventPhase + " ";
-      }, true);
-    }
+    addListenersToDOM(dom);
     dom.shadowH1.dispatchEvent(new Event("click", {composed: true}));
   },
-  expect: "div+1 slot+1 slotRoot+1 slotSpan+1 slotSlot+1 shadowComp+2 shadowRoot+1 shadowH1-2 shadowH1+2 shadowComp-2 ",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowComp +++++++-+-:1111121222",
   result: function () {
-    return res;
+    return res1 + res2 + ":" + res3;
   }
 }, {
   name: "propagation: composed: YES bubbles: YES",
   fun: function () {
-    res = "";
+    res1 = res2 = res3 = "";
     const dom = cleanDom();
-    for (let elName in dom) {
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "-" + e.eventPhase + " ";
-      }, {});
-      dom[elName].addEventListener("click", function (e) {
-        res += elName + "+" + e.eventPhase + " ";
-      }, true);
-    }
+    addListenersToDOM(dom);
     dom.shadowH1.dispatchEvent(new Event("click", {composed: true, bubbles: true}));
   },
-  expect: "div+1 slot+1 slotRoot+1 slotSpan+1 slotSlot+1 shadowComp+2 shadowRoot+1 shadowH1-2 shadowH1+2 shadowRoot-3 shadowComp-2 slotSlot-3 slotSpan-3 slotRoot-3 slot-3 div-3 ",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowRoot shadowComp slotSlot slotSpan slotRoot slot div +++++++-+-------:1111121223233333",
   result: function () {
-    return res;
+    return res1 + res2 + ":" + res3;
+  }
+}, {
+  name: "propagation2: composed: NO bubbles: NO",
+  fun: function () {
+    res1 = res2 = res3 = "";
+    const dom = cleanDom();
+    addListenersToDOM(dom);
+    dom.shadowComp.dispatchEvent(new Event("click"));
+  },
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122",
+  result: function () {
+    return res1 + res2 + ":" + res3;
+  }
+}, {
+  name: "propagation2: composed: NO bubbles: YES",
+  fun: function () {
+    res1 = res2 = res3 = "";
+    const dom = cleanDom();
+    addListenersToDOM(dom);
+    dom.shadowComp.dispatchEvent(new Event("click", {bubbles: true}));
+  },
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333",
+  result: function () {
+    return res1 + res2 + ":" + res3;
+  }
+}, {
+  name: "propagation2: composed: YES bubbles: NO",
+  fun: function () {
+    res1 = res2 = res3 = "";
+    const dom = cleanDom();
+    addListenersToDOM(dom);
+    dom.shadowComp.dispatchEvent(new Event("click", {composed: true}));
+  },
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122",
+  result: function () {
+    return res1 + res2 + ":" + res3;
+  }
+}, {
+  name: "propagation2: composed: YES bubbles: YES",
+  fun: function () {
+    res1 = res2 = res3 = "";
+    const dom = cleanDom();
+    addListenersToDOM(dom);
+    dom.shadowComp.dispatchEvent(new Event("click", {composed: true, bubbles: true}));
+  },
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333",
+  result: function () {
+    return res1 + res2 + ":" + res3;
   }
 }];
