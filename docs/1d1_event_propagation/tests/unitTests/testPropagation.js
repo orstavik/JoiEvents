924209f3ad7cd499f236a5c7ad5d38dd1af9d6ce
intertext.no <orstavik77@gmail.com>
2020-06-22 13:47:25 +0200

    cleaning tests

--- docs/1d1_event_propagation/tests/unitTests/testPropagation.js
+++ docs/1d1_event_propagation/tests/unitTests/testPropagation.js
@@ -11,11 +11,13 @@ function domWithListeners() {
       res1 += elName + " ";
       res2 += "-";
       res3 += e.eventPhase;
+      Promise.resolve().then(()=>res3+= ".")
     }, {});
     dom[elName].addEventListener("click", function (e) {
       res1 += elName + " ";
       res2 += "+";
       res3 += e.eventPhase;
+      Promise.resolve().then(()=>res3+= ".")
     }, true);
   }
   return dom;
@@ -35,42 +37,42 @@ function getResults(){
 
 export const testProp = [{
   name: "dispatchEvent: shadowH1: composed NO bubbles NO async NO",
-  expect: "shadowRoot shadowH1 shadowH1 +-+:122",
+  expect: "shadowRoot shadowH1 shadowH1 +-+:122...",
   fun: makeTestFunction("shadowH1"),
   result:getResults
 }, {
   name: "dispatchEvent: shadowH1: composed: NO bubbles: YES",
-  expect: "shadowRoot shadowH1 shadowH1 shadowRoot +-+-:1223",
+  expect: "shadowRoot shadowH1 shadowH1 shadowRoot +-+-:1223....",
   fun: makeTestFunction("shadowH1", {bubbles: true}),
   result:getResults
 }, {
   name: "dispatchEvent: shadowH1: composed: YES bubbles: NO",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowComp +++++++-+-:1111121222",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowComp +++++++-+-:1111121222..........",
   fun: makeTestFunction("shadowH1", {composed: true}),
   result:getResults,
 }, {
   name: "dispatchEvent: shadowH1: composed: YES bubbles: YES",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowRoot shadowComp slotSlot slotSpan slotRoot slot div +++++++-+-------:1111121223233333",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowRoot shadowH1 shadowH1 shadowRoot shadowComp slotSlot slotSpan slotRoot slot div +++++++-+-------:1111121223233333................",
   fun: makeTestFunction("shadowH1", {composed: true, bubbles: true}),
   result:getResults,
 }, {
   name: "dispatchEvent2: composed: NO bubbles: NO",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122.......",
   fun: makeTestFunction("shadowComp"),
   result:getResults,
 }, {
   name: "dispatchEvent2: composed: NO bubbles: YES",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333............",
   fun: makeTestFunction("shadowComp", {bubbles: true}),
   result:getResults,
 }, {
   name: "dispatchEvent2: composed: YES bubbles: NO",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp +++++-+:1111122.......",
   fun: makeTestFunction("shadowComp", {composed: true}),
   result:getResults,
 }, {
   name: "dispatchEvent2: composed: YES bubbles: YES",
-  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333",
+  expect: "div slot slotRoot slotSpan slotSlot shadowComp shadowComp slotSlot slotSpan slotRoot slot div +++++-+-----:111112233333............",
   fun: makeTestFunction("shadowComp", {bubbles: true, composed: true}),
   result:getResults,
 }];
