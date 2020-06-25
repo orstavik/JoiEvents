1977fc35a07b9083e7d092eaa3dcd69048dd4516
intertext.no <orstavik77@gmail.com>
2020-06-25 09:37:08 +0200

    adding once tests

--- docs/1d1_event_propagation/tests/unitTests/testOnce.js
+++ docs/1d1_event_propagation/tests/unitTests/testOnce.js
@@ -3,11 +3,11 @@ import {cleanDom} from "./useCase1.js";
 let res;
 
 function a(e) {
-  res += "c";
+  res += "a";
 }
 
 function b(e) {
-  res += "d";
+  res += "b";
 }
 
 function c(e) {
@@ -19,7 +19,50 @@ function getResult() {
 }
 
 export const testOnce = [{
-  name: "once: removeEventListener",
+  name: "once: true",
+  fun: function () {
+    res = "";
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", a, {once: true});
+    h1.dispatchEvent(new Event("click"));
+    h1.dispatchEvent(new Event("click"));
+    h1.dispatchEvent(new Event("click", {bubbles: true, composed: true}));
+  },
+  expect: "a",
+  result: getResult
+}, {
+  name: "once: false",
+  fun: function () {
+    res = "";
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", a, {once: false});
+    h1.dispatchEvent(new Event("click"));
+    h1.dispatchEvent(new Event("click"));
+    h1.dispatchEvent(new Event("click", {bubbles: true, composed: true}));
+  },
+  expect: "aaa",
+  result: getResult
+}, {
+  name: "once: adding same listener object to the same target",
+  fun: function () {
+    res = "";
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", a, {capture: true, once: true});
+    h1.addEventListener("click", a, true);
+    h1.addEventListener("click", a, {once: true});
+    h1.addEventListener("click", a, {once: 1});
+    h1.addEventListener("click", a);
+
+    h1.addEventListener("click", b, {once: true});
+    h1.addEventListener("click", b);
+
+    h1.dispatchEvent(new Event("click", {bubbles: true}));
+    h1.dispatchEvent(new Event("click"));
+  },
+  expect: "aab",
+  result: getResult
+}, {
+  name: "once: removeEventListener twice",
   fun: function () {
     res = "";
     const h1 = document.createElement("h1");
@@ -32,145 +75,58 @@ export const testOnce = [{
   expect: "",
   result: getResult
 }, {
-  name: "once: add two event listeners, one with once and one without",
+  name: "once: two multiple listeners to the same target, not once added first",
   fun: function () {
     res = "";
     const h1 = document.createElement("h1");
     h1.addEventListener("click", a);
     h1.addEventListener("click", a, {once: true});
-    h1.addEventListener("click", b, {once: true});
     h1.addEventListener("click", b);
+    h1.addEventListener("click", b, {once: true});
+    h1.dispatchEvent(new Event("click", {bubbles: true}));
+    h1.dispatchEvent(new Event("click", {bubbles: true}));
+    h1.dispatchEvent(new Event("click", {bubbles: true}));
+  },
+  expect: "ababab",
+  result: getResult
+}, {
+  name: "once: {capture:true, once: true}",
+  fun: function () {
+    res = "";
+    const h1 = document.createElement("h1");
+    h1.addEventListener("click", a);
+    h1.addEventListener("click", b, {once: true});
+    h1.addEventListener("click", c, {capture: true, once: true});
 
     h1.dispatchEvent(new Event("click", {bubbles: true}));
     h1.dispatchEvent(new Event("click", {bubbles: true}));
     h1.dispatchEvent(new Event("click", {bubbles: true}));
   },
-  expect: "abaa",
+  expect: "abcaa",  //c is after ab, because the h1 is in the lowestmost AT_TARGET phase
   result: getResult
-},{
-  name: "once: 1",
+}, {
+  name: "once: slotted element",          //todo move these last two tests to the test of propagation
   fun: function () {
     res = "";
-    let dom = cleanDom();
-    dom.div.addEventListener("click", a, {once: 1});
-    dom.div.addEventListener("click", a, {once: true});
-    dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-    dom.div.dispatchEvent(new Event("click"));
+    const dom = cleanDom(true);
+    dom.slot.addEventListener("click", a, {once: true});
+    dom.slot.addEventListener("click", b);
+    dom.slot.dispatchEvent(new Event("click", {bubbles: true}));
+    dom.shadowComp.dispatchEvent(new Event("click", {bubbles: true}));
+    dom.shadowComp.dispatchEvent(new Event("click", {bubbles: true}));
+  },
+  expect: "abbb",
+  result: getResult
+}, {
+  name: "once: composed: false does not propagates through shadowRoot",
+  fun: function () {
+    res = "";
+    const dom = cleanDom(true);
+    dom.shadowH1.addEventListener("click", a, {once: true});
+    dom.div.addEventListener("click", b);
+    dom.shadowH1.dispatchEvent(new Event("click", {bubbles: true}));
+    dom.shadowH1.dispatchEvent(new Event("click", {bubbles: true}));
   },
   expect: "a",
   result: getResult
-},
-
-  {
-    name: "once: removeEventListener",
-    fun: function () {
-      res = "";
-      let dom = cleanDom();
-      dom.div.addEventListener("click", a, {once: true});
-      dom.div.removeEventListener("click", a);
-      dom.div.addEventListener("click", a);
-      dom.div.removeEventListener("click", a, {once: true});
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "",
-    result: getResult
-  },
-
-  {
-    name: "once: add two event listeners, one with once and one without",
-    fun: function () {
-      res = "";
-      let dom = cleanDom();
-      dom.div.addEventListener("click", a);
-      dom.div.addEventListener("click", a, {once: true});
-      dom.div.addEventListener("click", b, {once: true});
-      dom.div.addEventListener("click", b);
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "abaa",
-    result: getResult
-  },
-
-  {
-    name: "once: two listeners to the same element",
-    fun: function () {
-      res = "";
-      let dom = cleanDom();
-      dom.div.addEventListener("click", a);
-      dom.div.addEventListener("click", a, {once: true});
-      dom.div.addEventListener("click", a, {once: true});
-      dom.div.addEventListener("click", b);
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.div.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "ababab",
-    result: getResult
-  },
-
-  {
-    name: "once: {capture:true, bubbles: true, once: true}",
-    fun: function () {
-      res = "";
-      const h1 = document.createElement("h1");
-
-
-      h1.addEventListener("click", a);
-      h1.addEventListener("click", b, {once: true});
-      // h1.addEventListener("click", d, {once: true});
-      h1.addEventListener("click", c, {capture: true, bubbles: true, once: true});
-
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "abcaa",
-    result: getResult
-  },
-
-  {
-    name: "once: {capture:true, bubbles: true, once: true}",
-    fun: function () {
-      res = "";
-      const h1 = document.createElement("h1");
-      h1.addEventListener("click", a);
-      h1.addEventListener("click", b, {once: true});
-      h1.addEventListener("click", c, {capture: true, bubbles: true, once: true});
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-      h1.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "abcaa",
-    result: getResult
-  },
-
-  {
-    name: "once: slotted element",
-    fun: function () {
-      res = "";
-      const dom = cleanDom(true);
-      dom.slot.addEventListener("click", a, {once: true});
-      dom.slot.addEventListener("click", b);
-      dom.slot.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.shadowComp.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.shadowComp.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "abbb",
-    result: getResult
-  },
-
-  {
-    name: "once: does not propagates through shadowRoot",
-    fun: function () {
-      res = "";
-      const dom = cleanDom(true);
-      dom.shadowH1.addEventListener("click", a, {once: true});
-      dom.div.addEventListener("click", b);
-      dom.shadowH1.dispatchEvent(new Event("click", {bubbles: true}));
-      dom.shadowH1.dispatchEvent(new Event("click", {bubbles: true}));
-    },
-    expect: "a",
-    result: getResult
-  }];
+}];
