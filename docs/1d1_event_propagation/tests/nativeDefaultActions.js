030db75f6f1569d90a1fb6514413ab6627cc93e5
intertext.no <orstavik77@gmail.com>
2020-07-29 19:27:51 +0200

    cleaning

--- docs/1d1_event_propagation/tests/nativeDefaultActions.js
+++ docs/1d1_event_propagation/tests/nativeDefaultActions.js
@@ -1,34 +1,49 @@
-const listOfDefaultActions = [
-  {
-    event: "click?button=0&isTrusted=true",   //the event has the same syntax as url query parameters
-    element: "a[href]",        //the element has the same syntax as querySelectors, limited to direct children ">"
-    exclusive: true,
-    method: function (currentTarget) {
-      document.open(currentTarget.getAttribute("href"));
-    }
-  }, {
-    event: "auxclick?button=1",
-    element: "a[href]",
-    exclusive: true,
-    method: function (currentTarget) {
-      document.open(currentTarget.getAttribute("href"), "_BLANK");
-    }
-  }, {
-    event: "click?button=0",
-    element: "input[type=checkbox]",
-    exclusive: true,
-    method: function (currentTarget) {
-      currentTarget.checked = !currentTarget.checked;
-    }
-  }, {
-    event: "click?button=0",
-    element: "details > summary",
-    exclusive: true,
-    method: function (currentTarget) {
-      currentTarget.toggle();
-    }
+// the event has the same syntax as url query parameters
+// the element has similar syntax as querySelectors:
+//  1. "," comma separated alternatives split and run as separate processes
+//  2. ">" relationships across several elements must be specified via direct children separators.
+//     But the parent > child relationship can span 3 or more levels, ie. "select > optgroup > option", or
+//     "form > * > input".. There is a lack of support for button and forms..
+//     todo we need to support " " separated parent child relationships
+//  3. Then, each entry is matched with each link in the parent child relationship
+//  *. no default action parent child relationship can cross shadowDOM borders.
+
+const listOfDefaultActions = [{
+  eventQuery: "click?button=0&isTrusted=true",
+  elementQuery: "a[href]",
+  exclusive: true,
+  method: function (parent) {
+    document.open(parent.getAttribute("href"));
   }
-];
+}, {
+  eventQuery: "auxclick?button=1",
+  elementQuery: "a[href]",
+  exclusive: true,
+  method: function (parent) {
+    document.open(parent.getAttribute("href"), "_BLANK");
+  }
+}, {
+  eventQuery: "click?button=0",
+  elementQuery: "input[type=checkbox]",
+  exclusive: true,
+  method: function (parent) {
+    parent.checked = !parent.checked;
+  }
+}, {
+  eventQuery: "mousedown?button=0",
+  elementQuery: "select > option, select > optgroup > option",
+  exclusive: true,
+  method: function (parent, child) {
+    parent.requestSelect(child);
+  }
+}, {
+  eventQuery: "click?button=0",
+  elementQuery: "details > summary",
+  exclusive: true,
+  method: function (currentTarget) {
+    currentTarget.toggle();
+  }
+}];
 
 function makeEventFilter(eventQuery) {
   const question = eventQuery.indexOf("?");
@@ -46,29 +61,31 @@ function makeEventFilter(eventQuery) {
   };
 }
 
-function makeElementFilter(elementQuery) {
-  const matchers = elementQuery.split(">").reverse();
-  return function (e) {
-    const targets = e.composedPath(); //this implies access to closed shadowRoots
-    targetLoop: for (let i = 0; i < targets.length; i++) {
-      let j = 0;
-      for (; j < matchers.length; j++) {
-        let matcher = matchers[j];
-        const checkTarget = targets[i + j];
-        if (!(checkTarget instanceof HTMLElement) || !checkTarget.matches(matcher))
-          continue targetLoop;
+function makeElementFilter(elementQueries) {
+  const queries = elementQueries.split(",").map(query => query.split(">").reverse());
+  return function matchParentChild(e) {
+    for (let matchers of queries) {
+      const targets = e.composedPath();                     //this implies access to closed shadowRoots
+      targetLoop: for (let i = 0; i < targets.length; i++) {
+        let j = 0;
+        for (; j < matchers.length; j++) {
+          let matcher = matchers[j];
+          const checkTarget = targets[i + j];
+          if (!(checkTarget instanceof HTMLElement) || !checkTarget.matches(matcher))
+            continue targetLoop;
+        }
+        return [targets[i], targets[i + j]];
       }
-      return targets[i];
     }
-    return null;
+    return [];
   };
 }
 
 function parseListOfDefaultActions(list) {
-  return list.map(function ({event, element, exclusive, method}) {
+  return list.map(function ({eventQuery, elementQuery, exclusive, method}) {
     return {
-      event: makeEventFilter(event),
-      element: makeElementFilter(element),
+      eventQuery: makeEventFilter(eventQuery),
+      elementQuery: makeElementFilter(elementQuery),
       exclusive,
       method
     };
@@ -78,14 +95,14 @@ function parseListOfDefaultActions(list) {
 const parsedListOfDefaultActions = parseListOfDefaultActions(listOfDefaultActions);
 
 export function lowestExclusiveNativeDefaultActions(e) {
-  const targetToNative = parsedListOfDefaultActions.map(function ({event, element, exclusive, method}) {
-    const element1 = element(e);
-    const event1 = event(e);
+  const targetToNative = parsedListOfDefaultActions.map(function ({eventQuery, elementQuery, exclusive, method}) {
+    const matchesEvent = eventQuery(e);
+    const [parentMatch, childMatch] = elementQuery(e);
     return {
-      event: event1,
-      element: element1,
+      event: matchesEvent,
+      element: parentMatch,
       exclusive,
-      task: method.bind(null, element1)
+      task: method.bind(null, parentMatch, childMatch)
     };
   });
   const filteredNative = targetToNative.filter(({event, element}) => !!event && !!element);
