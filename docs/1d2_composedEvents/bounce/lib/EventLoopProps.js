86fc380df516377718bf1eedf87a76096ef2ae8d
intertext.no <orstavik77@gmail.com>
2021-04-08 08:45:09 +0200

    babystep ready for unit tests

--- docs/1d2_composedEvents/bounce/lib/EventLoopProps.js
+++ docs/1d2_composedEvents/bounce/lib/EventLoopProps.js
@@ -1,14 +1,16 @@
-//this adds properties to the Event so that we can get the correct state information from it.
 export {tick} from "./EventLoop.js";
+
+//this adds properties to the Event so that we can get the correct state information from it.
+//rule #x: adding phase 4 for event finished propagating.
 
 Object.defineProperties(Event.prototype, {
   'currentTarget': {
     get: function () {
       const frame = this.__frame;
-      return frame.listener === -1 ? null : frame.bouncedPath[frame.doc].path[frame.target];
+      return frame.listener === -1 ? null : frame.contexts[frame.doc].path[frame.target];
     }
   },
-  'eventPhase': {     //adding phase 4 for event finished propagating.
+  'eventPhase': {
     get: function () {
       const frame = this.__frame;
       return frame ? frame.phase : 0;
@@ -17,19 +19,19 @@ Object.defineProperties(Event.prototype, {
   'path': {
     get: function () {
       const frame = this.__frame;
-      return frame.listener === -1 ? [] : frame.bouncedPath[frame.doc].path.slice();
+      return frame.listener === -1 ? [] : frame.contexts[frame.doc].path.slice();
     }
   },
   'currentDocument': {
     get: function () {
       const frame = this.__frame;
-      return frame.listener === -1 ? null : frame.bouncedPath[frame.doc].root;
+      return frame.listener === -1 ? null : frame.contexts[frame.doc].root;
     }
   },
   'bouncedPath': {
     get: function () { //returns a copy of the bouncedPath (preserving the original bouncedPath immutable).
       const frame = this.__frame;
-      return frame.bouncedPath.map(({root, path}) => ({root, path: path.slice()}));
+      return frame.contexts.map(({root, path}) => ({root, path: path.slice()}));
     }
   }
 });
